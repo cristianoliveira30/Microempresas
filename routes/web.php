81945ed9bd67ada6dbd143243b4f4dc32a8d6102
MarcusCalderaro <marcuseduardo54@gmail.com>
2025-05-09 11:08:36 -0300

    merge

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -19,8 +18,6 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', fn() => Inertia::render('Dashboard'))->name('dashboard');
-    Route::get('/produto', fn() => Inertia::render('Produto/Produto', [
-        'produtos' => app(ProductController::class)->index(),
-    ])->name('produto'));
+    Route::get('/dashboard', fn () => Inertia::render('Dashboard'))->name('dashboard');
+    Route::get('/produto', fn () => Inertia::render('Produto/Produto'))->name('produto');
 });
